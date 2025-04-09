29fe699b2d63254b787c26fa71b20d7d0b9aa142
Camilo <ch385593@gmail.com>
2025-04-09 02:32:31 -0500

    agregamos recupear contraseña, moficando el usuario.py de routes y creamos el html recuperar

--- routes/usuario.py
+++ routes/usuario.py
@@ -5,6 +5,7 @@ import yagmail
 import threading
 from dotenv import load_dotenv
 import os
+import random, string
 
 load_dotenv()
 
@@ -64,3 +65,37 @@ def cerrarSesion():
     session.clear()
     mensaje="sesion cerrada"
     return render_template("iniciarSesion.html", mensaje=mensaje)
+
+def generar_contraseña(n=8):
+    caracteres = string.ascii_letters + string.digits
+    return ''.join(random.choice(caracteres) for _ in range(n))
+
+@app.route("/recuperar/", methods=["GET","POST"])
+def recuperar():
+    mensaje = ""
+    try:
+        if request.method == 'POST':
+            usuario = request.form['txtusuario']
+            correo_form = request.form['txtcorreo']
+
+            # Verifica que el usuario exista
+            user = Usuario.objects(usuario=usuario, correo=correo_form).first()
+            if user:
+                nueva_pass = generar_contraseña()
+                user.password = nueva_pass  
+                user.save()
+
+                correo_envio = os.environ.get("CORREO")
+                clave_envio = os.environ.get("ENVIAR_CORREO")
+                email = yagmail.SMTP(correo_envio, clave_envio)
+                asunto = "Recuperación de contraseña"
+                mensaje = f"Hola {user.nombres}, tu nueva contraseña es: {nueva_pass}"
+
+                email.send(to=user.correo, subject=asunto, contents=mensaje)
+                return render_template("iniciarSesion.html", mensaje="Nueva contraseña enviada al correo")
+            else:
+                mensaje = "Usuario o correo incorrecto"
+    except Exception as e:
+        mensaje = str(e)
+
+    return render_template("Recuperar.html", mensaje=mensaje)
